1993ed720558292a40960435ea8cce23c5efb261
Behrang Saeedzadeh <behrangsa@gmail.com>
2020-06-06 00:37:49 +1000

    Added banner

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("================");
+    System.out.println("==== Banner ====");
+    System.out.println("================");
+
     for (int i = 0; i < 3; i++) {
       System.out.println("Hello, World");
     }
